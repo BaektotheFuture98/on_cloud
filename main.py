692d61f5a6a9dfc18273aff00002295a6e54f393
BSMITSME <sun4461637@gmail.com>
2023-08-28 12:02:49 +0900

    SeonMin's modify

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from kafka import KafkaConsumer, KafkaProducer
 from PIL import Image
 from io import BytesIO
 import os
-from EasyOCR.easyocr import easyocr
+import easyocr
 import json
 import numpy as np
 import papago as pg
+<<<<<<< HEAD
 import cv2
 import base64
 import traceback
@@ -15,13 +16,28 @@ reader = easyocr.Reader(lang_list = ['ko', 'en'], gpu=True) # recog_network = 't
 
 consumer = KafkaConsumer('topic',
                          bootstrap_servers = sc.KafkaConsumer_bootstrap_servers,
+=======
+import base64
+
+#reader = easyocr.Reader(lang_list = ['ko','en','ja'], recog_network = 'trocr', gpu=True)
+reader = easyocr.Reader(['en','ko'])
+
+consumer = KafkaConsumer('test2',
+                         bootstrap_servers = ['localhost:9092'],
+>>>>>>> 0ab1d24 (SeonMin's modify)
                          auto_offset_reset = 'latest'
                          )
 
 producer = KafkaProducer(acks='all',
                          compression_type='gzip',
+<<<<<<< HEAD
                          bootstrap_servers= sc.KafkaProducer_bootstrap_servers)
 print ('Model is Ready')
+=======
+                         bootstrap_servers=['localhost:9092'])
+                        #  bootstrap_servers=['52.91.126.82:9092','34.232.53.143:9092','100.24.240.6:9092'])
+
+>>>>>>> 0ab1d24 (SeonMin's modify)
 def on_send_success(record_metadata) : 
     print("메시지 전송 성공. Topic:", record_metadata.topic, "Partition:", record_metadata.partition, "Offset:", record_metadata.offset)
 
@@ -83,6 +99,7 @@ class NpEncoder(json.JSONEncoder):
         return super(NpEncoder, self).default(obj)
     
 def processincomsumer(message):
+<<<<<<< HEAD
     global reader
     message = base64.b64decode(message)
     img = BytesIO(message)
@@ -102,6 +119,22 @@ def processincomsumer(message):
     #os.remove(f"pro.jpg")
     
   #  print(result)
+=======
+    print(type(message))
+#    # global reader
+#     img = BytesIO(message)
+#     img = Image.open(img).convert("RGB")
+#     img.save(f"pro.jpg")
+    img = base64.b64decode(message)
+    # 바운딩 박스, 텍스트, 임계값
+    with open('pro.png', 'bw') as f :
+        f.write(img)
+    result = reader.readtext(f"pro.png")
+
+    os.remove(f"pro.png")
+
+    print(result)
+>>>>>>> 0ab1d24 (SeonMin's modify)
 
     re_ko, re_en, re_jp = switch_json(result)
     print(f"""re_ko : {re_ko}""")
@@ -110,6 +143,7 @@ def processincomsumer(message):
     producer.send('korean', re_ko.encode('utf-8')).add_callback(on_send_success).add_errback(on_send_error)
     producer.send('english', re_en.encode('utf-8')).add_callback(on_send_success).add_errback(on_send_error)
     producer.send('japan', re_jp.encode('utf-8')).add_callback(on_send_success).add_errback(on_send_error)
+
 
 
 if __name__ == '__main__' : 
